650c721885b335637abf7d47907aad4d8d3a8424
MoNRaSpGit <ju4nrsuarez@gmail.com>
2024-12-25 23:38:37 -0300

    fdsfdf

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap primero
 import './Css/tarjetas.css'; // Tus estilos después
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // Cambia a HashRouter
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
