76df574c9277b0649ce3d0f7df0bcfe894b31e43
MoNRaSpGit <ju4nrsuarez@gmail.com>
2024-12-25 21:51:28 -0300

    dsfsfsd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import UserList from './Componentes/UserList';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import VisitCounter from "./Componentes/VisitCounter";
+
 
 const App = () => {
   return (
     <div>
       <h1 className="text-center mt-4">Chamba YA!!!!</h1>
       <UserList />
-      <VisitCounter />
+      
     </div>
   );
 };
